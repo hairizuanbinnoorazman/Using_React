b12eeee666b8258f55efe3407559802176245580
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-01-08 00:31:57 +0800

    feat: Wire up the whole app together

--- hn-replica/hn-replica-frontend/src/App.js
+++ hn-replica/hn-replica-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Posts from './components/posts';
+import NavBar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
+        <NavBar/>
+        <Posts/>
       </div>
     );
   }
